fbf588d6fbc4024a36798c33cdd6c4a475ac928b
Tobias <thaegi@gmx.de>
2021-10-26 17:52:54 +0200

    Fix gulp sass, update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require( 'gulp' );
-const sass = require( 'gulp-sass' );
+const sass = require( 'gulp-sass' )(require('node-sass'));
 const rename = require( 'gulp-rename' );
 const terser = require( 'gulp-terser' );
 const autoprefixer = require( 'gulp-autoprefixer' );
